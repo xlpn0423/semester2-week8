4cafc455f2bffbbb4176ef2c0baeac6a444b6225
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-03-23 10:11:57 +0000

    1

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -7,7 +7,28 @@
 int main( void ) {
 
     Point p1 = makePoint(0.0,0.0);
-    
+
+    Point p2 = makePoint(1.0,1.0);
+
+    Point p3 = makePoint(3.0,4.0);
+
+    Point p4 = makePoint(5.0,5.0);
+
+    Point p5 = makePoint(5.0,5.0);
+
+    printf("p1: (%.1f, %.1f)\n", p1.x, p1.y);
+
+    printf("p2: (%.1f, %.1f)\n", p2.x, p2.y);
+
+    Line l1( Point p1, Point p2 );
+
+    Line l2( Point p1, Point p4 );
+
+    Triangle makeTriangle( Point p1, Point p2, Point p3 )
+
+    printf("are p1 and p4 same:%s\n", samePoint(p1, p4) ? "yes" : "no");
+
+    printf("Line l1(p1-p2) length:%.2f\n", lineLength(l1));
     // test your code by calling the functions and printing the output 
     
     return 0;
--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -6,10 +6,79 @@
 
 Point makePoint( float x, float y ) {
     Point new;
+
+    new.x = x;
+
+    new.y = y;
+
     // implementation
     return new;
 }
 
+Line makeLine( Point p1, Point p2 ) {
+
+    Line new;
+
+    new.p[0] = p1;
+
+    new.p[1] = p2;
+
+    return new;
+}
+
+Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
+
+    Triangle new;
+
+    new.p[0] = p1;
+
+    new.p[1] = p2;
+
+    new.p[2] = p3;
+
+    return new;
+}
+
+float lineLength( Line l ) {
+
+   float dx = p1.x + p2.x;
+
+   float dy = p1.y + p2.y;
+
+   return sqrt(dx*dx+dy*dy);
+}
+
+float triangleArea( Triangle t ) {
+
+   float a = lineLength(makeLine(t.p[0],t.p[1]));
+
+   float b = lineLength(makeLine(t.p[1],t.p[2]));
+
+   float c = lineLength(makeLine(t.p[0],t.p[2]));
+
+   float s = (a + b + c) / 2;
+
+   float area = sqrt(fabs(s*(s-a)*(s-b)*(s-c)));
+
+   return area;
+}
+
+bool samePoint( Point p1, Point p2 ) {
+
+    float dx = fabs(p1.x - p2.x);
+
+    float dy = fabs(p1.y - p2.y);
+
+    return p1.x==p2.x||p1.y==p2.y;
+}
+
+bool pointInLine( Point p, Line l) {
+    return samePoint(p,l.p[0]) || samePoint(p,l.p[1]);
+}
+
+bool pointInTriangle( Point p, Triangle t ) {
+    return samePoint(p,t.p[0]) || samePoint(p,t.p[1] || samePoint(p,t.p[2]);
+}
 // complete other functions below
 // - start with stubs as above
 // - compile regularly to test syntax
