319c006d1ef0a6409df2f2f1d62f7479faec003a
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-03-23 10:42:38 +0000

    3

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -20,15 +20,14 @@ int main( void ) {
 
     printf("p2: (%.1f, %.1f)\n", p2.x, p2.y);
 
-    Line l1( Point p1, Point p2 );
+    Line l1 = makeLine(p1, p2);
 
-    Line l2( Point p1, Point p4 );
+    Line l2 = makeLine(p1, p4);
 
-    Triangle makeTriangle( Point p1, Point p2, Point p3 )
 
     printf("are p1 and p4 same:%s\n", samePoint(p1, p4) ? "yes" : "no");
 
-    printf("Line l1(p1-p2) length:%.2f\n", lineLength(l1));
+    printf("Line l1(p1,p2) length:%.2f\n", lineLength(l1));
     // test your code by calling the functions and printing the output 
     
     return 0;
